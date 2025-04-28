86f5612352ac2502914ab6bebddff8ef7495325c
UtkarshPingale <132140825+UtkarshPingale@users.noreply.github.com>
2025-04-28 19:23:07 +0530

    nlp

--- a5.py
+++ a5.py
@@ -15,3 +15,25 @@ for word in doc:
 print()
 for word in doc.ents:
     print(f"{word.text:<15} : {word.label_}")
+
+# ----------------------------------------------------------------------
+
+import nltk
+from nltk import word_tokenize, pos_tag, ne_chunk
+
+nltk.download('punkt')
+nltk.download('maxent_ne_chunker')
+nltk.download('words')
+
+text = "Utkarsh Pingale was born on 8:30 am in Pune and go to AISSMSIOIT college"
+
+tokens = word_tokenize(text)
+tags = pos_tag(tokens)
+
+entities = ne_chunk(tags)
+
+for chunk in entities:
+    if isinstance(chunk, nltk.Tree):
+        print(f"{' '.join([word for word, tag in chunk])} -> {chunk.label()}")
+    else:
+        print(f"{chunk[0]} -> None")
